5dde6db0c7742f697f1461d1011b237efd0c62f8
uahmad417 <18u.ahmad@gmail.com>
2022-09-15 17:10:03 +0500

    implemented functionality that attempts to clone ipconfig functionality in the network resource

--- resources/network.py
+++ resources/network.py
@@ -1,26 +1,27 @@
-from xml.dom.pulldom import PROCESSING_INSTRUCTION
 import psutil
-from socket import AF_INET, socket
-from socket import SOCK_DGRAM
-from socket import SOCK_STREAM
-from socket import AF_INET6
+from .constants import PROTOCOL_MAP, NETSTAT_OUTPUT_FORMAT, IPCONFIG_OUTPUT_FORMAT
 
 def netstat():
-    protocol_map = {
-        (AF_INET, SOCK_STREAM): 'tcp',
-        (AF_INET6, SOCK_STREAM): 'tcp6',
-        (AF_INET, SOCK_DGRAM): 'udp',
-        (AF_INET6, SOCK_DGRAM): 'udp6',
-    }
-
-    network_output_format = '{:<10}{:<35}{:<20}{:<15}{:<15}'
-    print(network_output_format.format('Proto','Local Address','Foreign Address','State','PID'))
-
+    print(NETSTAT_OUTPUT_FORMAT.format('Proto','Local Address','Foreign Address','State','PID'))
     processes = {proc.info['pid']: proc.info['name'] for proc in psutil.process_iter(['pid','name'])}
     for socket_ in psutil.net_connections():
-        protocol = protocol_map[(socket_.family,socket_.type)]
+        protocol = PROTOCOL_MAP[(socket_.family,socket_.type)]
         local_address = f'{socket_.laddr.ip}:{socket_.laddr.port}'
         remote_address = f'{socket_.raddr.ip}:{socket_.raddr.port}' if socket_.raddr else '0.0.0.0:0'
         state = socket_.status
         pid_process_name = f'{socket_.pid}/{processes.get(socket_.pid)}'
-        print(network_output_format.format(protocol,local_address,remote_address,state,pid_process_name))
+        print(NETSTAT_OUTPUT_FORMAT.format(protocol,local_address,remote_address,state,pid_process_name))
+
+def ifconfig():
+    return
+
+def ipconfig():
+    interface_info = psutil.net_if_addrs()
+    
+    for interface in interface_info:
+        print('\n'+interface+'\n')
+        mac_address = interface_info[interface][0].address if len(interface_info[interface]) == 3 else ''
+        ipv4_address = interface_info[interface][1].address if len(interface_info[interface]) == 3 else interface_info[interface][0].address
+        netmask = interface_info[interface][1].netmask if len(interface_info[interface]) == 3 else interface_info[interface][0].address
+        ipv6_address = interface_info[interface][2].address if len(interface_info[interface]) == 3 else interface_info[interface][1].address
+        print(IPCONFIG_OUTPUT_FORMAT.format(mac_address,ipv4_address,netmask,ipv6_address))
