94d4ba3a8d4aeba8df77f43680dd26eae92dfbe7
uahmad417 <18u.ahmad@gmail.com>
2022-09-16 16:31:10 +0500

    Added memory option functionality now the tool displays memory information when the memory option is specified

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from package.monitor_cli import cli
-from package.resources import network,process,cpu
+from package.resources import network,process,cpu,memory
 
 if __name__ == '__main__':
     args = cli()
     FUNCTION_MAP = {
     'network': network.ipconfig if 'interface' in args and args['interface'] == True else network.netstat,
     'process': process.ps_verbose if 'verbose' in args and args['verbose'] == True else process.ps,
-    'cpu': cpu.cpu_info
+    'cpu': cpu.cpu_info,
+    'memory': memory.main
     }
+    
     f = FUNCTION_MAP.get(args['resource'])
     f()
