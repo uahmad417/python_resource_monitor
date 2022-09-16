4e3cdc5fd2221490896b815237af9307b8105428
uahmad417 <18u.ahmad@gmail.com>
2022-09-16 16:01:16 +0500

    Modified the cli to accept cpu argument. modified the function mapper to display cpu info when cpu option is selected

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from package.monitor_cli import cli
-from package.resources import network,process
+from package.resources import network,process,cpu
 
 if __name__ == '__main__':
     args = cli()
     FUNCTION_MAP = {
     'network': network.ipconfig if 'interface' in args and args['interface'] == True else network.netstat,
-    'process': process.ps_verbose if 'verbose' in args and args['verbose'] == True else process.ps
+    'process': process.ps_verbose if 'verbose' in args and args['verbose'] == True else process.ps,
+    'cpu': cpu.cpu_info
     }
     f = FUNCTION_MAP.get(args['resource'])
     f()
