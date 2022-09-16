b61502d8844c825ace4239c39b183d8392895b25
uahmad417 <18u.ahmad@gmail.com>
2022-09-16 17:38:35 +0500

    Added parser arguments for disk option

--- package/monitor_cli.py
+++ package/monitor_cli.py
@@ -3,47 +3,57 @@ import argparse
 
 def cli():
     parser = argparse.ArgumentParser(
-    description='Simple Python Resource Monitor',
-    prog='monitor.py',
-    usage='%(prog)s [RESOURCE] [OPTIONS]',
-    epilog='Pass the resource along with -h flag to view list of resource options')
+        description='Simple Python Resource Monitor',
+        prog='monitor.py',
+        usage='%(prog)s [RESOURCE] [OPTIONS]',
+        epilog='Pass the resource along with -h flag to view list of resource options')
 
     subparser = parser.add_subparsers(
-    dest='resource', 
-    required=True)
+        dest='resource', 
+        required=True)
 
     cpu_parser = subparser.add_parser(
-    'cpu', 
-    help='Display CPU Information')
+        'cpu', 
+        help='Display CPU Information')
 
     network_parser = subparser.add_parser(
-    'network',
-    help='Display network Information', 
-    usage='monitor.py network [OPTIONS]')
+        'network',
+        help='Display network Information', 
+        usage='monitor.py network [OPTIONS]')
     network_parser.add_argument(
-    '-i',
-    '--interface',
-    help='Display interface addreses', 
-    action='store_true')
+        '-i',
+        '--interface',
+        help='Display interface addreses', 
+        action='store_true')
 
     memory_parser = subparser.add_parser(
-    'memory', 
-    help='Display memory Information')
+        'memory', 
+        help='Display memory Information')
 
     disk_parser = subparser.add_parser(
-    'disk', 
-    help='Display disk Information')
+        'disk', 
+        help='Display disk Information')
+    disk_parser.add_argument(
+        '-p',
+        '--partition',
+        help='Show Disk Partitions',
+        action='store_true'
+    )
+    disk_parser.add_argument(
+        '-u',
+        '--usage',
+        help='Show Disk Usage information',
+        action='store_true'
+    )
 
     process_parser = subparser.add_parser(
-    'process',
-    usage='monitor.py process [OPTION]', 
-    help='Display process Information')
+        'process',
+        usage='monitor.py process [OPTION]', 
+        help='Display process Information')
     process_parser.add_argument(
         '-v',
         '--verbose',
         action='store_true',
         help='Display extended process information'
     )
-
-    args = parser.parse_args()
-    return vars(args)
+    return vars(parser.parse_args())
