4e0f6fc05081768ad8388f358fe611b908d67ca1
uahmad417 <18u.ahmad@gmail.com>
2022-09-16 16:26:47 +0500

    Created script to display memory related information

---
+++ package/resources/memory.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+import psutil
+from psutil._common import bytes2human
+
+def pprint_tuple(nt):
+    for name in nt._fields:
+        value = getattr(nt,name)
+        if name != 'percent':
+            value = bytes2human(value)
+        print('{:<15}:{:>15}'.format(name.capitalize(),value))
+    
+
+def main():
+    print('MEMORY\n----------')
+    pprint_tuple(psutil.virtual_memory())
+    print('\nSwap\n----------')
+    pprint_tuple(psutil.swap_memory())
+
+main()
