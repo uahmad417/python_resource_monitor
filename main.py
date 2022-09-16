9b98c7de06dcd6887b7af0b8a5175de18c5b8193
uahmad417 <18u.ahmad@gmail.com>
2022-09-16 17:37:21 +0500

    Added disk option to Function mapper modified the main program file to include functionality for the tool to show disk related info when specified with the option and its various switches

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from package.monitor_cli import cli
-from package.resources import network,process,cpu,memory
+from package.resources import network,process,cpu,memory,disk
 
 if __name__ == '__main__':
     args = cli()
@@ -7,8 +7,9 @@ if __name__ == '__main__':
     'network': network.ipconfig if 'interface' in args and args['interface'] == True else network.netstat,
     'process': process.ps_verbose if 'verbose' in args and args['verbose'] == True else process.ps,
     'cpu': cpu.cpu_info,
-    'memory': memory.main
+    'memory': memory.main,
+    'disk': disk.disk_partitions if 'partition' in args and args['partition'] == True else (disk.disk_usage if 'usage' in args and args['usage'] == True else disk.df) 
     }
-    
+
     f = FUNCTION_MAP.get(args['resource'])
     f()
